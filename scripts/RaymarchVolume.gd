faa9dd305bc868002edd768c3a6b9df9b11b13bf
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2025-08-26 21:33:47 -0400

    blackhole gravitational lensing, accretion disc next

--- scripts/RaymarchVolume.gd
+++ scripts/RaymarchVolume.gd
@@ -4,8 +4,18 @@ class_name RaymarchVolume
 
 @export var max_steps: int
 @export var max_step_size: float
+@export var max_distance: float
 
-func _process(delta: float):
+@export_range(0, 100000, 0.01, "exp") var gravity_constant: float
+
+@export var panorama: PanoramaSkyMaterial
+# @export var cubemap: Cubemap
+
+func _process(_delta: float):
 	var material = mesh.surface_get_material(0)
 	material.set_shader_parameter("MAX_STEPS", max_steps)
 	material.set_shader_parameter("MAX_STEP_SIZE", max_step_size)
+	material.set_shader_parameter("MAX_DISTANCE", max_distance)
+
+	material.set_shader_parameter("panorama", panorama.panorama)
+	material.set_shader_parameter("GRAVITY_CONSTANT", 1 / gravity_constant)
