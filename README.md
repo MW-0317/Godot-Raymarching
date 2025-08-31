56a373b5e5e7dac082f9efc7d0ba87e6f7ad015c
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2025-08-31 18:12:27 -0400

    Broken version, pushing for readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Godot-Raymarching
+Based on this [video](https://www.youtube.com/watch?v=dMohMW29gSM)
+
+# Current Ideas
+- [Dithering](https://www.youtube.com/watch?v=8wOUe32Pt-E) to recreate how the first rendered black hole looks
+- FFT to recreate [lens effects](https://www.youtube.com/watch?v=QWqb5Gewbx8)
--- scripts/RaymarchVolume.gd
+++ scripts/RaymarchVolume.gd
@@ -9,13 +9,25 @@ class_name RaymarchVolume
 @export_range(0, 100000, 0.01, "exp") var gravity_constant: float
 
 @export var panorama: PanoramaSkyMaterial
+@export var light: Light3D
+
+# @export var time = 0.0
 # @export var cubemap: Cubemap
 
-func _process(_delta: float):
-	var material = mesh.surface_get_material(0)
-	material.set_shader_parameter("MAX_STEPS", max_steps)
-	material.set_shader_parameter("MAX_STEP_SIZE", max_step_size)
-	material.set_shader_parameter("MAX_DISTANCE", max_distance)
+func _process(delta: float):
+    var material = mesh.surface_get_material(0)
+    material.set_shader_parameter("MAX_STEPS", max_steps)
+    material.set_shader_parameter("MAX_STEP_SIZE", max_step_size)
+    material.set_shader_parameter("MAX_DISTANCE", max_distance)
 
-	material.set_shader_parameter("panorama", panorama.panorama)
-	material.set_shader_parameter("GRAVITY_CONSTANT", 1 / gravity_constant)
+    material.set_shader_parameter("panorama", panorama.panorama)
+    material.set_shader_parameter("GRAVITY_CONSTANT", 1 / gravity_constant)
+
+    # print(delta)
+    # time += delta
+    var time = float(Time.get_ticks_msec()) / 1000
+    # time = 100
+    light.position = Vector3((2 + sin(3*time))*sin(time), 0, (2 + sin(3*time))*cos(time))
+    material.set_shader_parameter("light_position", light.position)
+    material.set_shader_parameter("light_color", light.light_color)
+    
